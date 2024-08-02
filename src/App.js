e5c608f054597db13b5f5c6478853464b5610490
shaithea05 <shaithea.alam@gmail.com>
2024-08-01 23:19:21 -0400

    starting weather app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import axios from 'axios'
 
 function App() {
+
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=boston&appid=1ca9063cc0a792252ba8bb70b97291d1`
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='container'>
+        <div className='top'>
+          <div className='location'>
+            <p>Boston</p>
+          </div>
+          <div className='temp'>
+            <h1>65°F</h1>
+          </div>
+          <div className='description'>
+            <p>Cloudy</p>
+          </div>
+        </div>
+
+        <div className='bottom'>
+          <div className='feels'>
+            <p>65°F</p>
+          </div>
+          <div className='humidity'>
+            <p>25%</p>
+          </div>
+          <div className='winds'>
+            <p>12MPH</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
